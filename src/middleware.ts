2589439408052ca2c3c3198ec70029dc8f841de9
Oliad Rundassa <runol@utschools.ca>
2025-07-20 01:53:20 -0400

    feat: added TOS & privacy page

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,6 +14,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|api/auth(?:/.*)?|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|api/auth(?:/.*)?|terms|privacy|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
