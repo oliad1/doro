e65a0ce228697c8d1080a020875476eaf88aa912
Oliad Rundassa <runol@utschools.ca>
2025-07-29 11:09:11 -0400

    fix: added error check for cookie API

--- src/APIClients/ProfilesAPIClient.ts
+++ src/APIClients/ProfilesAPIClient.ts
@@ -24,22 +24,26 @@ const getUser = async (): Promise<any> => {
     const { data, error } = await supabase.auth.getUser();
 
     if (error) {
-      console.log("Error:", error.message);
       throw new Error(`Error: ${error.message}`);
     }
 
     if (!data || !data.user) {
-      console.log("Error: user is null");
       throw new Error(`Error: user is null`);
     }
     
-    const cookiePostRes = await fetch('/api/cookies/user', {
+    const cookieRes = await fetch('/api/cookies/user', {
       method: 'POST',
       headers: {
 	'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
     });
+
+    const {error: cookieErr } = await cookieRes.json();
+
+    if (cookieErr) {
+      throw new Error(`Error: ${cookieErr}`);
+    }
 
     return data.user;
   } catch (error) {
