a864884b164d5b3d3f1f20bf25cef39dd9a7c964
Oliad Rundassa <runol@utschools.ca>
2025-10-01 16:29:44 -0400

    feat: sortable, searchable, visibility-toggling tables for assessments

---
+++ frontend/src/app/(dashboard)/course/[id]/columns.tsx
@@ -0,0 +1,205 @@
+"use client"
+
+import { useState } from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Badge } from "@/components/ui/badge";
+import { format } from "date-fns"; 
+import { Calendar as CalendarIcon, Pencil, Eraser, Trash2, Check } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { CHANGE_COURSE_HEADER } from "@/constants/DialogConstants";
+import { DataTableColumnHeader } from "@/components/ui/data-table-column-header";
+
+export type Assessment = {
+  id: string;
+  name: string;
+  dropped: boolean;
+  optional: boolean;
+  weight: number;
+  symbol: string | null;
+  grade: number | null;
+  date: Date | undefined;
+  default_date: Date | undefined;
+  modified_date: Date | undefined;
+}
+
+interface ColumnProps {
+  formula: boolean;
+  editingState: Record<string, boolean>,
+  inputState: Record<string, string>,
+  toggleEditing: (id: string, value: string, oldValue: string) => void,
+  handleGradeChange: (e: string, id: string) => void,
+  editTimeRef: React.RefObject<HTMLInputElement | null>,
+  upsertDate: (id: string, value: Date | undefined) => void,
+  deleteDate: (id: string) => void,
+  deleteGrade: (id: string) => void,
+}
+
+export const columns = ({editingState, handleGradeChange, inputState, toggleEditing, editTimeRef, upsertDate, deleteDate, deleteGrade}: ColumnProps): ColumnDef<Assessment>[] => [
+  {
+    accessorKey: "name",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Name" />
+    ),
+    cell: ({ row }) => {
+      return <div className="font-medium">
+	{row.getValue("name")}
+	{(row.original.dropped) && (
+	  <Badge variant="secondary" className="ml-2" >Dropped</Badge>
+	)}
+	{(row.original.optional) && (
+	  <Badge variant="outline" className="ml-2" >Optional</Badge>
+	)}
+      </div>
+    }
+  },
+  {
+    accessorKey: "weight",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Weighting" />
+    ),
+    cell: ({ row }) => {
+      console.log(row.original.symbol);
+      return <div className="flex flex-row gap-2 items-center">
+	{row.getValue("weight")}
+	{row.original.symbol && (
+	  <Button size="icon" variant="outline" className="p-0 size-5">
+	    {row.original.symbol}
+	  </Button>
+	)}
+      </div>
+    },
+  },
+  {
+    accessorKey: "grade",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Grade" />
+    ),
+    cell: ({ row }) => {
+      const [input, setInput] = useState<string>(row.getValue("grade") ?? "");
+      return <div className="flex w-min max-w-sm justify-center items-center gap-2">
+	<Input
+	  type="number"
+	  onChange={(e) => setInput(e.target.value)}
+	  disabled={!(editingState[row.original.id as string]) as boolean}
+	  value={input}
+	  className="min-w-24"
+	  placeholder="Grade"
+	/>
+	<Button
+	  size="icon"
+	  className="size-8 px-5"
+	  onClick={()=>{
+	    toggleEditing(row.original.id, input, row.getValue("grade"));
+	  }}
+	  variant="secondary"
+	>
+	  {editingState[row.original.id] ?
+	    <Check />
+	    : <Pencil/>
+	  }
+	</Button>
+	<Button
+	  size="icon"
+	  className="size-8 px-5"
+	  onClick={()=>deleteGrade(row.original.id)}
+	  variant="secondary"
+	>
+	  <Eraser />
+	</Button>
+      </div>
+    },
+  },
+  {
+    accessorKey: "date",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Due Date" />
+    ),
+    cell: ({ row }) => {
+      const [editDate, setEditDate] = useState<Date | undefined>(undefined);
+      const date = row.original.date;
+      const now = new Date(Date.now());
+      const datePassed = now > date! && date;
+      const editDatePassed = now > editDate! && editDate;
+
+      return <div className="flex flex-row gap-2 items-center">
+	<Dialog>
+	  <DialogContent>
+	    <CHANGE_COURSE_HEADER/>
+	    <div className="flex flex-col sm:flex-row items-center sm:items-start justify-center gap-2">
+	      <Calendar
+		mode="single"
+		selected={editDate}
+		onSelect={(selectedDate) => setEditDate(selectedDate)}
+		captionLayout="dropdown"
+	      />
+	      <div className="flex flex-col items-center gap-y-2">
+		<Input
+		  ref={editTimeRef}
+		  type="time"
+		  defaultValue={editDate ? format(editDate, "HH:mm") : "23:59"}
+		  className="bg-background appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
+		/>
+		<Button
+		  variant="secondary" 
+		  className={"font-normal"+(!editDatePassed ? '' : " opacity-50")}
+		>
+		  <CalendarIcon/>
+		  {editDate ? format(editDate, ("MMM dd yyyy")) : "Select Date"}
+		</Button>
+	      </div>
+	    </div>
+	    <DialogFooter className="sm:justify-between">
+	      <DialogClose asChild>
+		<Button variant="ghost">Cancel</Button>
+	      </DialogClose>
+	      <div className="flex flex-col sm:flex-row items-center gap-2 w-full sm:w-min">
+		<Button variant="ghost" onClick={()=>{setEditDate(undefined)}} className="w-full sm:w-min text-destructive hover:text-destructive hover:bg-destructive/20 ">
+		  Clear
+		</Button>
+		<Button 
+		  onClick={()=>{
+		    console.log("INPUT", editTimeRef, editTimeRef?.current, editTimeRef?.current?.value);
+		    if (editTimeRef.current && editTimeRef.current.value.includes(":")) {
+		      const base = editDate ? new Date(editDate) : new Date();
+		      const updated = new Date(base);
+		      const [hour, minute] = editTimeRef?.current?.value.split(":").map((item) => Number.parseInt(item));
+		      updated.setHours(hour, minute);
+		      setEditDate(updated);
+		      upsertDate(row.original.id, updated);
+		    }
+		  }}
+		  className="w-full sm:w-min">
+		  Confirm 
+		</Button>
+	      </div>
+	    </DialogFooter>
+	  </DialogContent>
+	  <DialogTrigger asChild>
+	    <Button
+	      variant="secondary" 
+	      className={"font-normal"+(!datePassed ? '' : " opacity-50")}
+	      onClick={()=>{setEditDate(date)}}
+	      //disabled={now > date && row.original.due_date}
+	    >
+	      <CalendarIcon/>
+	      {date ? format(date, ("MMM dd yyyy h:mm a")) : "Select Date"}
+	    </Button>
+	  </DialogTrigger>
+	  {(!!row.original.modified_date) && (
+	    <Button
+	      size="icon"
+	      className="size-8 px-5"
+	      onClick={()=>deleteDate(row.original.id)}
+	      variant="secondary"
+	    >
+	      <Trash2/>
+	    </Button>
+	  )}
+	</Dialog>
+      </div>
+    },
+  },
+]
--- frontend/src/components/Course/GradeTable.tsx
+++ frontend/src/components/Course/GradeTable.tsx
@@ -1,22 +1,17 @@
 import { useState, useEffect, useRef } from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardAction } from "@/components/ui/card";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Input } from "@/components/ui/input";
-import { Badge } from "@/components/ui/badge";
-import { Calendar } from "@/components/ui/calendar";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { Pencil, Check, Eraser, SquareFunction, Calendar as CalendarIcon, Trash2 } from "lucide-react";
-import { COURSE_TITLE, COURSE_BIO, COURSE_ASSIGNMENTS } from "@/constants/SkeletonConstants";
-import { CHANGE_COURSE_HEADER } from "@/constants/DialogConstants";
+import { DataTable } from "@/components/ui/data-table";
+import { columns } from "@/app/(dashboard)/course/[id]/columns";
+import { SquareFunction } from "lucide-react";
+import { COURSE_TITLE, COURSE_BIO } from "@/constants/SkeletonConstants";
 import CourseInfoDialog from "@/components/Course/CourseInfoDialog";
 import { toast } from "sonner";
 import GradesAPIClient from "@/APIClients/GradesAPIClient";
 import DatesAPIClient from "@/APIClients/DatesAPIClient";
 import { getAssessmentName } from "@/utils/helpers";
-import { format } from "date-fns"; 
 
 interface GradeTableProps {
   isLoading: boolean,
@@ -30,7 +25,6 @@ interface GradeTableProps {
 export default function GradeTable ({ isLoading, courseMetadata, upsertMetadata, deleteMetadata, enrollmentId, currFormula }: GradeTableProps) {
   const [editingState, setEditingState] = useState<Record<string, boolean>>({});
   const [inputState, setInputState] = useState<Record<string, string>>({});
-  const [editDate, setEditDate] = useState<Date | undefined>(undefined);
   const editTimeRef = useRef<HTMLInputElement>(null);
   const formula = !!courseMetadata?.conditions?.length;
 
@@ -138,11 +132,9 @@ export default function GradeTable ({ isLoading, courseMetadata, upsertMetadata,
     }
   };
 
-  const toggleEditing = (id: string) => {
-    if (editingState[id]) {
-      if (inputState[id]) {
-	upsertGrade(id, inputState[id]);
-      }
+  const toggleEditing = (id: string, value: string, oldValue: string) => {
+    if (editingState[id] && oldValue!=value) {
+      upsertGrade(id, value);
     }
     setEditingState((prevState) => ({
       ...prevState,
@@ -150,11 +142,11 @@ export default function GradeTable ({ isLoading, courseMetadata, upsertMetadata,
     }));
   };
 
-  const handleGradeChange = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
+  const handleGradeChange = (value: string, id: string) => {
     //console.log(e.target.value);
     setInputState((prevState) => ({
       ...prevState,
-      [id]: e.target.value.replace(/[^0-9.]/g, '')
+      [id]: value.replace(/[^0-9.]/g, '')
     }));
   }
 
@@ -203,170 +195,34 @@ export default function GradeTable ({ isLoading, courseMetadata, upsertMetadata,
       </CardHeader>
 
       <CardContent className="overflow-hidden w-[100%]">
-	    <ScrollArea className="whitespace-nowrap">
-	  <Table>
-	    <TableHeader>
-	      <TableRow>
-		<TableHead>Name</TableHead>
-		<TableHead>Weighting</TableHead>
-		<TableHead>Grade</TableHead>
-		<TableHead>Due Date</TableHead>
-	      </TableRow>
-	    </TableHeader>
-	  <TableBody>
-	    {isLoading
-	      ? <COURSE_ASSIGNMENTS />
-	      : <>
-	      {courseMetadata!.assessment_groups?.map((assessment_group: any) =>
-		  assessment_group.assessments.map((assessment: any) => {
-		    const date = !!assessment.dates.length 
-		      ? assessment.dates[0].date 
-			? new Date(assessment.dates[0].date) : undefined 
-		      : (assessment.due_date 
-			? (new Date(assessment.due_date)) : undefined);
-		    const now = new Date(Date.now())
-		    const datePassed = now > date! && date;
-		    const editDatePassed = now > editDate! && editDate;
-
-		    return <TableRow key={assessment.index}>
-		      <TableCell className="font-medium text-nowrap">
-			{getAssessmentName(assessment_group, assessment.index)}
-			{(assessment.dropped) && (
-			  <Badge variant="secondary" className="ml-2" >Dropped</Badge>
-			)}
-			{(assessment_group.optional || assessment_group.type?.includes("Bonus")) && (
-			  <Badge variant="outline" className="ml-2" >Optional</Badge>
-			)}
-		      </TableCell>
-
-		      <TableCell> 
-			<div className="flex flex-row gap-2 items-center">
-			  {(!formula ? assessment.weight : assessment.weight * assessment_group.weight).toPrecision(2).replace(/(?:\.0+|(\.\d+?)0+)$/, "$1")}
-			  {assessment_group?.condition_group_id?.symbol && (
-			    <Button size="icon" variant="outline" className="p-0 size-5">
-			      {assessment_group.condition_group_id.symbol}
-			    </Button>
-			  )}
-			</div>
-		      </TableCell>
-
-		      <TableCell className="flex w-min max-w-sm justify-center items-center gap-2">
-			<Input
-			  type="number"
-			  onChange={(e)=>handleGradeChange(e, assessment.id)}
-			  disabled={!(editingState[assessment.id as string]) as boolean}
-			  value={inputState[assessment.id] ?? ''}
-			  className="min-w-24"
-			  placeholder="Grade"
-			/>
-			<Button
-			  size="icon"
-			  className="size-8 px-5"
-			  onClick={()=>toggleEditing(assessment.id)}
-			  variant="secondary"
-			>
-			  {editingState[assessment.id] ?
-			    <Check />
-			    : <Pencil/>
-			  }
-			</Button>
-			<Button
-			  size="icon"
-			  className="size-8 px-5"
-			  onClick={()=>deleteGrade(assessment.id)}
-			  variant="secondary"
-			>
-			  <Eraser />
-			</Button>
-		      </TableCell>
-
-		      <TableCell className="w-min">
-			<div className="flex flex-row gap-2 items-center">
-			  <Dialog>
-			    <DialogContent>
-			      <CHANGE_COURSE_HEADER/>
-			      <div className="flex flex-col sm:flex-row items-center sm:items-start justify-center gap-2">
-				<Calendar
-				  mode="single"
-				  selected={editDate}
-				  onSelect={(selectedDate) => setEditDate(selectedDate)}
-				  captionLayout="dropdown"
-				/>
-				<div className="flex flex-col items-center gap-y-2">
-				  <Input
-				    ref={editTimeRef}
-				    type="time"
-				    defaultValue={editDate ? format(editDate, "HH:mm") : "23:59"}
-				    className="bg-background appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
-				  />
-				  <Button
-				    variant="secondary" 
-				    className={"font-normal"+(!editDatePassed ? '' : " opacity-50")}
-				  >
-				    <CalendarIcon/>
-				    {editDate ? format(editDate, ("MMM dd yyyy")) : "Select Date"}
-				  </Button>
-				</div>
-			      </div>
-			      <DialogFooter className="sm:justify-between">
-				<DialogClose asChild>
-				  <Button variant="ghost">Cancel</Button>
-				</DialogClose>
-				<div className="flex flex-col sm:flex-row items-center gap-2 w-full sm:w-min">
-				  <Button variant="ghost" onClick={()=>{setEditDate(undefined)}} className="w-full sm:w-min text-destructive hover:text-destructive hover:bg-destructive/20 ">
-				    Clear
-				  </Button>
-				  <Button 
-				    onClick={()=>{
-				      console.log("INPUT", editTimeRef, editTimeRef?.current, editTimeRef?.current?.value);
-				      if (editTimeRef.current && editTimeRef.current.value.includes(":")) {
-					const base = editDate ? new Date(editDate) : new Date();
-					const updated = new Date(base);
-					const [hour, minute] = editTimeRef?.current?.value.split(":").map((item) => Number.parseInt(item));
-					updated.setHours(hour, minute);
-					setEditDate(updated);
-					upsertDate(assessment.id, updated);
-				      }
-				    }}
-				    className="w-full sm:w-min">
-				    Confirm 
-				  </Button>
-				</div>
-			      </DialogFooter>
-			    </DialogContent>
-			    <DialogTrigger asChild>
-			      <Button
-				variant="secondary" 
-				className={"font-normal"+(!datePassed ? '' : " opacity-50")}
-				onClick={()=>{setEditDate(date)}}
-				//disabled={now > date && assessment.due_date}
-			      >
-				<CalendarIcon/>
-				{date ? format(date, ("MMM dd yyyy h:mm a")) : "Select Date"}
-			      </Button>
-			    </DialogTrigger>
-			    {(!!assessment.dates.length) && (
-			      <Button
-				size="icon"
-				className="size-8 px-5"
-				onClick={()=>deleteDate(assessment.id)}
-				variant="secondary"
-			      >
-				<Trash2/>
-			      </Button>
-			    )}
-			  </Dialog>
-			</div>
-		      </TableCell>
-		    </TableRow>
-		  })
-		)}
-		</>
-	    }
-	  </TableBody>
-	</Table>
-	      <ScrollBar orientation="horizontal" />
-	    </ScrollArea>
+	<DataTable 
+	  columns={columns({
+	    formula,
+	    editingState,
+	    inputState,
+	    editTimeRef,
+	    upsertDate,
+	    deleteDate,
+	    toggleEditing,
+	    deleteGrade,
+	    handleGradeChange,
+	  })}
+	  data={
+	    courseMetadata ? courseMetadata!.assessment_groups?.flatMap((assessment_group: any) =>
+	      assessment_group.assessments.map((assessment: any) => ({
+		id: assessment.id,
+		name: getAssessmentName(assessment_group, assessment.index),
+		dropped: assessment.dropped,
+		optional: assessment_group.optional || assessment_group.type?.includes("Bonus"),
+		weight: (!formula ? assessment.weight : assessment.weight * assessment_group.weight).toPrecision(2).replace(/(?:\.0+|(\.\d+?)0+)$/, "$1"),
+		symbol: assessment_group?.condition_group_id?.symbol ?? null,
+		grade: inputState[assessment.id],
+		date: (assessment.dates?.length ? new Date(assessment.dates[0].date) : undefined) ?? (assessment.due_date ? new Date(assessment.due_date) : undefined),
+		default_date: assessment.due_date ? new Date(assessment.due_date) : undefined,
+		modified_date: assessment.dates?.length ? new Date(assessment.dates[0].date) : undefined,
+	      }))
+	    ) : []}
+	/>
       </CardContent>
     </Card>
   );
